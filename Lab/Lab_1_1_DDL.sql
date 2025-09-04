dd5b40b514ba5cbc1af06c1137cd3ee0df9a93e7
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-09-04 11:20:54 +0545

    Create Lab_1_1_DDL.sql
    creating database of employee.

---
+++ Lab/Lab_1_1_DDL.sql
@@ -0,0 +1,7 @@
+create emp(
+  emp_id int primary key not null auto_increment,
+  emp_name varchar(20),
+  emp_username varchar(10),
+  emp_password varchar(16),
+  emp_address varchar(20) not null);
+  
