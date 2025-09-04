78d11438e1637823f5bfe827516aaa9001a699cc
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-09-04 19:09:09 +0545

    Update Lab_1_1_DDL.sql

--- Lab/Lab_1_1_DDL.sql
+++ Lab/Lab_1_1_DDL.sql
@@ -71,11 +71,4 @@ CREATE TABLE student (
 );
 --- Output: In your MYSQl
 --*************************************************************************
--- To create Table in Database 
-CREATE TABLE emp (
-  emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  emp_name VARCHAR(20),
-  emp_username VARCHAR(10),
-  emp_password VARCHAR(16),
-  emp_address VARCHAR(20) NOT NULL
-);
+
