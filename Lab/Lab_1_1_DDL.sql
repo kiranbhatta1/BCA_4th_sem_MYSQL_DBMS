5529af299bf2e84edb24ecb6e3d1b2f8a7e61ec1
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-09-04 11:22:32 +0545

    Update Lab_1_1_DDL.sql

--- Lab/Lab_1_1_DDL.sql
+++ Lab/Lab_1_1_DDL.sql
@@ -1,7 +1,7 @@
-create emp(
-  emp_id int primary key not null auto_increment,
-  emp_name varchar(20),
-  emp_username varchar(10),
-  emp_password varchar(16),
-  emp_address varchar(20) not null);
-  
+CREATE TABLE emp (
+  emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  emp_name VARCHAR(20),
+  emp_username VARCHAR(10),
+  emp_password VARCHAR(16),
+  emp_address VARCHAR(20) NOT NULL
+);
