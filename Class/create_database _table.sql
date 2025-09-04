573c976d2282a2eea88fd7d90816312fa2ea02a0
Kiran Bhatta <bhattakiran1111@gmail.com>
2025-09-04 19:10:16 +0545

    Create create_database _table.sql

---
+++ Class/create_database _table.sql
@@ -0,0 +1,8 @@
+-- To create Table in Database 
+CREATE TABLE emp (
+  emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  emp_name VARCHAR(20),
+  emp_username VARCHAR(10),
+  emp_password VARCHAR(16),
+  emp_address VARCHAR(20) NOT NULL
+);
